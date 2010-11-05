c4b1fe24913c187d1b884c8dc34941ffbb6568b0
Sridhar Ratnakumar <sridharr@activestate.com>
2010-11-05 14:18:30 -0700

    check_output for python2.6

--- venv.py
+++ venv.py
@@ -196,7 +196,7 @@ def _workaround_virtualenv_bugs(py):
     # move user readline.so out of the way
     # http://bitbucket.org/ianb/virtualenv/issue/64
     try:
-        readline_path = subprocess.check_output(
+        readline_path = check_output(
             [py, '-c', 'import readline; print(readline.__file__)'],
             stderr=subprocess.PIPE).strip()
     except subprocess.CalledProcessError:
@@ -213,3 +213,40 @@ def _workaround_virtualenv_bugs(py):
         if replace:
             print('Moving %s back; virtualenv bug #64' % readline_path)
             os.rename(readline_path + '.oow', readline_path)
+
+
+
+# check_output for python2.6
+def check_output(*popenargs, **kwargs):
+    r"""Run command with arguments and return its output as a byte string.
+
+    If the exit code was non-zero it raises a CalledProcessError.  The
+    CalledProcessError object will have the return code in the returncode
+    attribute and output in the output attribute.
+
+    The arguments are the same as for the Popen constructor.  Example:
+
+    >>> check_output(["ls", "-l", "/dev/null"])
+    'crw-rw-rw- 1 root root 1, 3 Oct 18  2007 /dev/null\n'
+
+    The stdout argument is not allowed as it is used internally.
+    To capture standard error in the result, use stderr=STDOUT.
+
+    >>> check_output(["/bin/sh", "-c",
+    ...               "ls -l non_existent_file ; exit 0"],
+    ...              stderr=STDOUT)
+    'ls: non_existent_file: No such file or directory\n'
+    """
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
+
